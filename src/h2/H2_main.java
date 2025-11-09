d2026804eecba96ccf4b40c9d5947336acbd956f
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-09 18:41:36 +0100

    H2 bearbeitet

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -2,12 +2,19 @@ package h2;
 
 public class H2_main {
     public static void main(String[] args) {
-        boolean x = true;
-        boolean y = true;
-        boolean a = true;
-        boolean b = true;
-        boolean c = true;
-        int input = 0;
-
+        int input = 10;
+        boolean x = input == 10 || input == 11;
+        boolean y = input == 1 || input == 11;
+        boolean E1 = x == true;
+        boolean E2 = y == true;
+        boolean E3 = x != y;
+        boolean a = E1 && E2;
+        boolean b = E1 || E3;
+        boolean c = y == false;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
     }
 }
