4b1fd9e8a079cf670d05bbc65bcf8af6d77ca525
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-12 15:01:35 +0100

    p3 bearbeitet

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -1,4 +1,34 @@
 package p3;
 
 public class P3_main {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = true;
+        System.out.println(!(x != y && x == y));
+        //immer true
+        System.out.println(x || true);
+        //immer true
+        System.out.println(x && false);
+        //immer false
+        System.out.println((x && !y) || (!x));
+        //unterschiedlich
+        x = true;
+        y = false;
+        System.out.println(!(x != y && x == y));
+        System.out.println(x || true);
+        System.out.println(x && false);
+        System.out.println((x && !y) || (!x));
+        x = false;
+        y = true;
+        System.out.println(!(x != y && x == y));
+        System.out.println(x || true);
+        System.out.println(x && false);
+        System.out.println((x && !y) || (!x));
+        x = false;
+        y = false;
+        System.out.println(!(x != y && x == y));
+        System.out.println(x || true);
+        System.out.println(x && false);
+        System.out.println((x && !y) || (!x));
+    }
 }
